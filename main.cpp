a2cfd22b1df297ffdc0f1502565e11159a7a2586
Kostya Proger <40338606+KostyaProger@users.noreply.github.com>
2018-06-19 13:10:04 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+int fow;
+cout << "You Hacked this human" << endl;
+cout << "His login is Admin" << endl;
+cout << "Please hacked is a password" << endl;
+cout << "You have 4 passwords for hacking" << endl;
+cin >> fow;
+if (fow == 1) {
+cout << "this password isn't hacked his profile." << endl;
+} if (fow == 2) {
+cout << "this password is hacked his profile.  You win! His password = 1053162, and his money it's your" << endl;
+} if (fow == 3) {
+cout << "this password isn't hacked his profile." << endl;
+} if (fow == 4) {
+cout << "this password isn't hacked his profile." << endl;
+} 
+cout << "Game Over" << endl;
+
+return 0;
+}
